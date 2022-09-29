7828431ccd82cc4bd6ba30eb1f48b1ac565d12a7
Thiago Sanches <thiago.sanches0703@gmail.com>
2022-09-28 22:51:16 -0300

    adicionando imagem de resposta

--- functions.py
+++ functions.py
@@ -3,16 +3,14 @@ import matplotlib.pyplot as plot
 import cv2
 
 
-
 def colorPallete(fileaddress):
-    print('filename = ', fileaddress)
     ct = ColorThief(fileaddress)
     total = 6
     palette = list(set(ct.get_palette(color_count=total)))
     return palette
 
-def creatingMask(filename,low,high):
-    print("filename em creatingMask= ",filename)
+
+def creatingMask(filename, low, high):
     img = cv2.imread(filename)
     img = cv2.resize(img, (900, 650), interpolation=cv2.INTER_CUBIC)
 
@@ -22,9 +20,14 @@ def creatingMask(filename,low,high):
     mask = cv2.inRange(imgHSV, low, high)
     mask = 255-mask
 
+    cv2.imwrite('static/images/mask.jpg', mask)
+
     return mask
 
-def responseImage(image,mask):
-    res = cv2.bitwise_and(image, image, mask=mask)
-    return res
 
+def responseImage(filename, mask):
+    img = cv2.imread(filename)
+    img = cv2.resize(img, (900, 650), interpolation=cv2.INTER_CUBIC)
+    res = cv2.bitwise_and(img, img, mask=mask)
+
+    cv2.imwrite('static/images/response.jpg', res)
