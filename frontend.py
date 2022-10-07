1a6476c7a19432eb02c45b1d48f93f03bb05c238
Thiago Sanches <thiago.sanches0703@gmail.com>
2022-10-07 00:24:53 -0300

    recebendo cores no backend

--- frontend.py
+++ frontend.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, request, redirect, url_for
 import os
 from functions import *
 import numpy as np
+import colorsys
 
 
 app = Flask(__name__)
@@ -13,9 +14,10 @@ filename = 0
 absolute_path = 0
 
 
-@app.route('/home', methods=["GET", "POST"])
+@app.route('/', methods=["GET", "POST"])
 def upload_image():
     if request.method == "POST":
+
         image = request.files['file']
 
         if image.filename == '':
@@ -59,9 +61,23 @@ def get_colors():
 @app.route('/result', methods=["GET", "POST"])
 def results():
     if request.method == "POST":
-
+        color = request.form.get('color').split(',')
+        hsvColor = colorsys.rgb_to_hsv(
+            int(color[0]), int(color[1]), int(color[2]))
+        print(hsvColor)
+        print(hsvColor[0])
+        print(hsvColor[1])
+        print(hsvColor[2])
+        low = np.array(
+            [int(hsvColor[0]), int(hsvColor[1]), int(hsvColor[2])-100])
+        high = np.array([int(hsvColor[0]), int(hsvColor[1]), int(hsvColor[2])])
         low_green = np.array([25, 52, 72])
         high_green = np.array([102, 255, 255])
+        print()
+        print("low = ", low)
+        print("high = ", high)
+        print("low_green = ", low_green)
+        print("high_green = ", high_green)
 
         # criando mascara
         mask = creatingMask(absolute_path, low_green, high_green)
